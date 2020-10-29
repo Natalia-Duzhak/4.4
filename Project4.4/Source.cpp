6fe553d505bf9f7b077432249f7ce630e5393fae
наталя ДУЖАК <Duzhak.N@gmail.com>
2020-10-29 20:06:29 +0300

    4.4

---
+++ Project4.4/Source.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, y, R;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "R = "; cin >> R;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x <= -R)
+			y = R;
+		else
+			if (-R < x && x <= R)
+				y = R - sqrt(R * R - x * x);
+			else
+				if (R < x && x <= 6)
+					y = R + (-3 - R) / (6 - R) * (x - R);
+				else
+					y = -3 * (x - 6);
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
